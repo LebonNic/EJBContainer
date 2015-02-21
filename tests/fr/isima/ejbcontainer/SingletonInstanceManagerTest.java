bb60797af01b9164ffc0370057651540c8312138
LebonNic <nicolas.prugne@orange.fr>
2015-02-21 17:17:02 +0100

    Corrects side effects on a test

--- tests/fr/isima/ejbcontainer/SingletonInstanceManagerTest.java
+++ tests/fr/isima/ejbcontainer/SingletonInstanceManagerTest.java
@@ -9,9 +9,15 @@ public class SingletonInstanceManagerTest {
 
     @Test
     public void testSingletonBeanCreation() {
+        int initialSingletonBeanCount, finalSingletonBeanCount;
+
         IBean bean = EJBContainer.getInstance().createBean(IBean.class);
+        initialSingletonBeanCount = EJBContainer.getInstance().getSingletonInstanceManager().getInstanceCount();
         bean.businessMethod();
         bean.businessMethod();
-        assertEquals(EJBContainer.getInstance().getSingletonInstanceManager().getInstanceCount(), 1);
+        finalSingletonBeanCount = EJBContainer.getInstance().getSingletonInstanceManager().getInstanceCount();
+
+        //The value is 2 here because SingletonBean contains an other SingletonBean
+        assertEquals(finalSingletonBeanCount - initialSingletonBeanCount, 2);
     }
 }
