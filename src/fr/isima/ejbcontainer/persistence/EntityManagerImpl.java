4729863eedf9793af69e7b342713e493da4b8a92
LebonNic <nicolas.prugne@orange.fr>
2015-02-23 09:22:19 +0100

    Adds some logs

--- src/fr/isima/ejbcontainer/persistence/EntityManagerImpl.java
+++ src/fr/isima/ejbcontainer/persistence/EntityManagerImpl.java
@@ -5,10 +5,16 @@ import fr.isima.ejbcontainer.persistence.command.PersistCommand;
 import fr.isima.ejbcontainer.persistence.command.RemoveCommand;
 import fr.isima.ejbcontainer.transaction.TransactionManagerImpl;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @PersistenceContext(unitName = "mock")
 public class EntityManagerImpl implements EntityManager{
+    private static final Logger LOG = Logger.getLogger(EntityManagerImpl.class.getSimpleName());
+
     @Override
     public <T> T persist(T entity) {
+        LOG.log(Level.INFO, "Persist an object named \"" + entity.getClass().getSimpleName() + "\".");
         PersistCommand command  = new PersistCommand(entity);
         TransactionManagerImpl.getInstance().getCurrentTransaction().storeAndExecuteCommand(command);
         return entity;
@@ -16,6 +22,7 @@ public class EntityManagerImpl implements EntityManager{
 
     @Override
     public <T> T remove(T entity) {
+        LOG.log(Level.INFO, "Remove an object named \"" + entity.getClass().getSimpleName() + "\".");
         RemoveCommand command = new RemoveCommand(entity);
         TransactionManagerImpl.getInstance().getCurrentTransaction().storeAndExecuteCommand(command);
         return entity;
